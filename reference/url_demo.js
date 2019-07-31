30ecc2c1be8e2d2b31b8abe883fa04e7bc92c5b8
Lisa Trevis <l_trevis@hotmail.com>
2019-07-31 09:48:45 -0700

    finished up url demo

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -16,3 +16,13 @@ console.log(myUrl.pathname);
 
 // Serialized query
 console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+
+// Loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
